91b87036127575c019d36f123017f0cb800e6003
Allan Liebold <allan.liebold@gmail.com>
2017-12-25 15:58:42 -0800

    substring method

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,11 @@ public class HelloWorld {
         String greeting = "Hello world!";
         int stringLength = greeting.length();
         String uCase = greeting.toUpperCase();
+        String firstSub = greeting.substring(6);
         System.out.println(greeting);
         System.out.println(stringLength);
         System.out.println(uCase);
+        System.out.println(firstSub);
     }
 
 }
