c0462aa577b4a36982a8abad2f978605e38adcf3
Allan Liebold <allan.liebold@gmail.com>
2017-12-25 15:51:17 -0800

    uppercase string method

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         String greeting = "Hello world!";
         int stringLength = greeting.length();
+        String uCase = greeting.toUpperCase();
         System.out.println(greeting);
         System.out.println(stringLength);
+        System.out.println(uCase);
     }
 
 }
