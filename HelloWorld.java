2bbc5640f13d7be443d533b7e4479a01aa45d88d
Allan Liebold <allan.liebold@gmail.com>
2017-12-26 09:34:46 -0800

    imported Array methods

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,14 +2,19 @@ package helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String greeting = "Hello world!";
-        int stringLength = greeting.length();
-        String uCase = greeting.toUpperCase();
-        String firstSub = greeting.substring(6);
-        System.out.println(greeting);
-        System.out.println(stringLength);
-        System.out.println(uCase);
-        System.out.println(firstSub);
+      String greeting = "Hello world!";
+      int stringLength = greeting.length();
+      String uCase = greeting.toUpperCase();
+      String firstSub = greeting.substring(6);
+      int[] nums = {9, 2, 4, 8, 3};
+      int[] moreNums = {10, 3, 5, 9, 4};
+      System.out.println(greeting);
+      System.out.println(stringLength);
+      System.out.println(uCase);
+      System.out.println(firstSub);
+      System.out.println(Arrays.equals(nums, moreNums));
+      System.out.println(Arrays.toString(moreNums));
+      System.out.println(nums.length);
     }
 
 }
