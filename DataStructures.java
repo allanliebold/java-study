d98914a5989e09f3d09cf7f70b29aa5aba8d7e7f
Allan Liebold <allan.liebold@gmail.com>
2017-12-28 18:36:02 -0800

    created DataStructures file.

---
+++ DataStructures.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Game {
+
+  public static void main(String[] args) {
+    ArrayList<String> arcadeGames = new ArrayList<String>();
+    arcadeGames.add("Pac-Man");
+    arcadeGames.add("Galaga");
+    arcadeGames.add("Centipede");
+    arcadeGames.add("Dig Dug");
+
+    System.out.println(arcadeGames.size() + " games in the archive.");
+
+    for (String game: arcadeGames) {
+      System.out.println(game);
+    }
+  }
+
+}
