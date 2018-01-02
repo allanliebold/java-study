77a53a166fc4cde14eef2bdbd47292ea4f46c9cc
Allan Liebold <allan.liebold@gmail.com>
2018-01-02 15:13:16 -0800

    prints out message and calls displayMessage method

--- StaticDemo.java
+++ StaticDemo.java
@@ -18,6 +18,9 @@ public class StaticDemo {
     public static void main(String[] args) {
         MyClass sd = new MyClass();
 
+        System.out.println(sd.message);
+        sd.displayMessage();
+
         System.out.println(MyClass.greetings);
         MyClass.displayGreetings();
     }
