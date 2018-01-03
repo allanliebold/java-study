daafef3783eb3cbe7eeb72cb1eb7887026d84fc3
Allan Liebold <allan.liebold@gmail.com>
2018-01-03 15:25:58 -0800

    created ArrayMethodDemo file

---
+++ ArrayMethodDemo.java
@@ -0,0 +1,30 @@
+package arraymethoddemo;
+import java.util.Arrays;
+
+class MyClass{
+    public void printFirstElement(int[] a) {
+        System.out.println("The first element is " + a[0]);
+    }
+
+    public int[] returnArray() {
+        int[] a = new int[3];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i*2;
+        }
+        return a;
+    }
+}
+
+public class ArrayMethodDemo {
+
+    public static void main(String[] args) {
+        MyClass amd = new MyClass();
+
+        int[] myArray = {1, 2, 3, 4, 5};
+
+        amd.printFirstElement(myArray);
+        int[] myArray2 = amd.returnArray();
+
+        System.out.println(Arrays.toString(myArray2));
+    }
+}
