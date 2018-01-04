69343d0ee1bfaddce01fce7661d932baced3bd1e
Allan Liebold <allan.liebold@gmail.com>
2018-01-04 09:44:32 -0800

    added code to main class

--- ArrayMethodDemo.java
+++ ArrayMethodDemo.java
@@ -28,13 +28,26 @@ class MyClass{
 public class ArrayMethodDemo {
 
     public static void main(String[] args) {
-        MyClass amd = new MyClass();
+      MyClass amd = new MyClass();
 
-        int[] myArray = {1, 2, 3, 4, 5};
+      int[] myArray = {1, 2, 3, 4, 5};
 
-        amd.printFirstElement(myArray);
-        int[] myArray2 = amd.returnArray();
+      amd.printFirstElement(myArray);
+      int[] myArray2 = amd.returnArray();
 
-        System.out.println(Arrays.toString(myArray2));
+      int number = 2;
+
+      System.out.println(Arrays.toString(myArray2));
+      System.out.println("number before = " + number);
+      
+      amd.passPrimitive(number);
+
+      System.out.println("number after = " + number);
+      System.out.print("\n");
+      System.out.println("mArray[1] before = " + myArray[1]);
+
+      amd.passReference(myArray);
+
+      System.out.println("myArray[1] after = " + myArray[1]);
     }
 }
