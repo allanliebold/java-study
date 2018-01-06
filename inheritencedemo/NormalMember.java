029a67982ed84c49428a5c41d622ca747a283510
Allan Liebold <allan.liebold@gmail.com>
2018-01-06 11:41:37 -0800

    created NormalMember java class to demonstrate inheritence and method overriding.

---
+++ inheritencedemo/NormalMember.java
@@ -0,0 +1,18 @@
+package inheritencedemo;
+
+public class NormalMember extends Member {
+
+    public NormalMember() {
+        System.out.println("Child constructor with no parameters.");
+    }
+
+    public NormalMember(String pName, int pMemberID, int pMemberSince) {
+        super(pName, pMemberID, pMemberSince);
+        System.out.println("Child constructor with 3 parameters.");
+    }
+
+    @Override
+    public void calculateAnnualFee() {
+        annualFee = (1-0.01 * getDiscount()) * (100 + 12 * 30);
+    }
+}
