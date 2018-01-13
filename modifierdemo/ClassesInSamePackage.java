209f80a1037c9d14ccd1302effdcc9303f947b88
Allan Liebold <allan.liebold@gmail.com>
2018-01-13 08:40:10 -0800

    created modifier demo to show the three different access modifiers

---
+++ modifierdemo/ClassesInSamePackage.java
@@ -0,0 +1,31 @@
+package modifierdemo;
+
+public class ClassesInSamePackage {
+    //empty class
+}
+
+class ClassA extends ModifierDemo {
+    public void printMessages(){
+        System.out.println(publicNum);
+
+        System.out.println(protectedNum);
+
+        System.out.println(packagePrivateNum);
+
+        //System.out.println(privateNum);
+    }
+}
+
+class ClassB {
+    public void printMessages() {
+        ModifierDemo p = new ModifierDemo();
+
+        System.out.println(p.publicNum);
+
+        System.out.println(p.protectedNum);
+
+        System.out.println(p.packagePrivateNum);
+
+        //System.out.println(p.privateNum);
+    }
+}
